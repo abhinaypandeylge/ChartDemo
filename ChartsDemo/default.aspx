0c931d1f55c86d099279d1087aa64560ac5aad0f
Abhinay Pandey/LGSIA MPS-1(abhinay.pandey@lge.com) <abhinay.pandey@lge.com>
2020-05-29 11:36:04 +0530

    First updated code

---
+++ ChartsDemo/default.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChartsDemo._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="scripts/jquery-3.5.1.min.js"></script>
+    <script src="scripts/bootstrap.min.js"></script>
+    <script src="scripts/Chart.min.js"></script>
+    <script>
+        var projects=[];
+        var openCount=[];
+        var fixedCount=[];
+        var inprogressDuein=[];
+        var inprogressDueout=[];
+        function getData () {
+            $.ajax({
+                type: "POST",
+                url: "default.aspx/LoadData",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    data = response.d;
+                    for (var i = 0; i < data.length; i++) {
+                        projects.push(data[i].Project);
+                        openCount.push(data[i].CountOpen);
+                        fixedCount.push(data[i].CountFixed);
+                        inprogressDuein.push(data[i].CountInprogressDueIn);
+                        inprogressDueout.push(data[i].CountInprogressDueout);
+                    }
+                },
+                failure: function (response) {
+                    alert(response.d);
+                }
+            });
+        }
+        $(function () {
+            var response = getData();
+            var chart = new Chart(ctx, {
+                type: 'horizontalBar',                
+                  data:{
+                        labels: projects,
+                        datasets: [{
+                            label: 'Open',
+                            stack: 0,
+                            data: openCount,
+                            backgroundColor: "rgba(217,242,27,1)",
+                        },{
+                            label: 'Inprogress(Above Due Date)',
+                            stack: 1,
+                            data: inprogressDueout,
+                            backgroundColor: "rgba(242,6,2,1)",
+                        }, {
+                            label: 'Inprogress(Within Due Date)',
+                            stack: 1,
+                            data: inprogressDuein,
+                            backgroundColor: "rgba(17,240,203,1)",
+                        }, {
+                            label: 'Fixed',
+                            stack: 2,
+                            data: fixedCount,
+                            backgroundColor: "rgba(71,252,5,1)",
+                        }]
+                    },
+                options: {
+                    responsive: false,
+                    legend: {
+                        position: 'right'
+                    },
+                    scales: {
+                        xAxes: [{
+                            stacked: true
+                        }],
+                        yAxes: [{
+                            stacked: true
+                        }]
+                    }
+                }
+            });
+        
+
+           
+        });
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <canvas id="cty" width="700"></canvas>
+        
+        <canvas id="ctx" width="700"></canvas>
+
+    </form>
+</body>
+</html>
---
+++ ChartsDemo/default.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+using System.Web.Services;
+using System.Collections;
+
+namespace ChartsDemo
+{
+    public class value
+    {
+        public string Project { get; set; }
+        public string CountOpen { get; set; }
+        public string CountFixed { get; set; }
+        public string CountInprogressDueIn { get; set; }
+        public string CountInprogressDueout { get; set; }
+
+    }
+    public partial class _default : System.Web.UI.Page
+    {
+        private static OleDbConnection OledbConn;
+        private static OleDbCommand OledbCmd;
+        [WebMethod]
+        public static object LoadData()
+        {
+            InitializeOledbConnection("E:\\ChartsDemo\\ChartsDemo\\Data\\SampleData2.xls", ".xls");
+            DataTable tempTable = ReadFile();
+            IList<value> chartItem = new List<value>();
+
+            //var project = "Cayman 4G";
+            //int CountFixed = 4, CountOpen = 5, CountInprogressDueIn = 6, CountInprogressDueOut = 3;
+            
+            foreach (DataRow item in tempTable.Rows)
+            {
+                value Chart = new value();
+                Chart.Project = item[0].ToString();
+                Chart.CountOpen = tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Open" && x.Field<string>("Project")== Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault()==null ? "0": tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Open" && x.Field<string>("Project") == Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault();
+                Chart.CountInprogressDueIn = tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Inprogress Due in" && x.Field<string>("Project") == Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault() == null ? "0" : tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Inprogress Due in" && x.Field<string>("Project") == Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault();
+                
+                Chart.CountInprogressDueout = tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Inprogress Due out" && x.Field<string>("Project") == Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault() == null ? "0" : tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Inprogress Due out" && x.Field<string>("Project") == Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault();
+
+                Chart.CountFixed = tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Fixed" && x.Field<string>("Project") == Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault() == null ? "0" : tempTable.AsEnumerable().Where(x => x.Field<string>("Status") == "Fixed" && x.Field<string>("Project") == Chart.Project).Select(e => e.Field<string>("Count")).FirstOrDefault();
+                if (!(chartItem.Select(e=>e.Project).Contains(Chart.Project)))
+                {
+                    chartItem.Add(Chart);
+                }
+            }
+
+            return chartItem.Distinct();
+          
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                      
+        }
+
+         static void InitializeOledbConnection(string filename, string extrn)
+        {
+            string connString = "";
+
+            if (extrn == ".xls")
+            //Connectionstring for excel v8.0    
+            {
+                connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source='"+ filename + "';Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\"";
+            }
+            else
+            {
+                //Connectionstring fo excel v12.0    
+                connString = "Provider=Microsoft.Jet.OLEDB.12.0;Data Source=\""+ filename + "\";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1\"";
+            }
+
+            OledbConn = new OleDbConnection(connString);
+        }
+
+        private static DataTable ReadFile()
+        {
+            try
+            {
+
+                DataTable schemaTable = new DataTable();
+                OledbCmd = new OleDbCommand();
+                OledbCmd.Connection = OledbConn;
+                OledbConn.Open();
+                OledbCmd.CommandText = "Select * from [Output$]";
+                OleDbDataReader dr = OledbCmd.ExecuteReader();
+                DataTable ContentTable = null;
+                if (dr.HasRows)
+                {
+                    ContentTable = new DataTable();
+                    ContentTable.Columns.Add("Project", typeof(string));
+                    ContentTable.Columns.Add("Status", typeof(string));
+                    ContentTable.Columns.Add("Count", typeof(string));
+                    //ContentTable.Columns.Add("Project", typeof(string));
+
+                    while (dr.Read())
+                    {
+                        if (dr[0].ToString().Trim() != string.Empty && dr[1].ToString().Trim() != string.Empty && dr[2].ToString().Trim() != string.Empty && dr[0].ToString().Trim() != " " && dr[1].ToString().Trim() != " " && dr[2].ToString().Trim() != " ")
+                            ContentTable.Rows.Add(dr[0].ToString().Trim(), dr[1].ToString().Trim(), dr[2].ToString().Trim());
+
+                    }
+                }
+                dr.Close();
+                OledbConn.Close();
+                return ContentTable;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+    }
+}
